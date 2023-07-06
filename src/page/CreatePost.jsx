7ad4d775d2692d78b4069ce453575f76c9541ba1
Hamzamolik <asifh6721@gmail.com>
2023-07-06 21:52:09 +0100

    commit2

--- src/page/CreatePost.jsx
+++ src/page/CreatePost.jsx
@@ -21,7 +21,7 @@ const CreatePost = () => {
       setLoading(true)
 
       try {
-        const response = await fetch('https://delleback.vercel.app/api/v1/post', {
+        const response = await fetch('https://delle1.vercel.app/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -55,7 +55,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://delleback.vercel.app/api/v1/dalle', {
+        const response = await fetch('https://delle1.vercel.app/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
     const fetchpost = async () => {
       setloading(true)
       try {
-        const response = await fetch('https://delleback.vercel.app/api/v1/post', {
+        const response = await fetch('https://delle1.vercel.app/api/v1/post', {
           method: 'GET',
           headers: {
             'content-type': 'application/json',
